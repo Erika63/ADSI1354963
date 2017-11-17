803f24f7ac23bbbe36b57d56a45906a4a177831d
Erika Lorena Sapuy Diaz <erikalorena.096@gmail.com>
2017-11-17 14:26:41 -0500

    cambio 2 con vim

--- archivo2.php
+++ archivo2.php
@@ -3,6 +3,9 @@
 <title>moddificando en git</title>
 </head>
 <body>
- <h1>trabajar en vim es molesto
+<h1>trabajar en vim es molesto</h1>
+<h2>detesto esto !!</h2>
+
+<p>medio puedo escribir en vim</p>
 </body>
 </html>
